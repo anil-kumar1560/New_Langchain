a7434464c801e1ea44e9e7e9058564f8d1f43a0c
anil-kumar1560 <anilgummadidala20@gmail.com>
2025-01-29 08:04:51 -0500

    Adding Agent Folder

--- api/client.py
+++ api/client.py
@@ -7,9 +7,7 @@ def get_openai_response(input_text):
     return response.json()['output']['content']
 
 def get_ollama_response(input_text):
-    response=requests.post(
-    "http://localhost:8000/poem/invoke",
-    json={'input':{'topic':input_text}})
+    response=requests.post("http://localhost:8000/poem/invoke", json={'input':{'topic':input_text}})
 
     return response.json()['output']
 
